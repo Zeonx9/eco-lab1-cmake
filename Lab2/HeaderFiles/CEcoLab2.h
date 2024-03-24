2e23bcdd56d031deabbdf41be0d700d2536423d7
Zeonx9 <mushtukov.artem@mail.ru>
2024-03-24 17:17:49 +0300

    create Component CEcoLab2 to test aggregation of CEcoLab1

---
+++ Lab2/HeaderFiles/CEcoLab2.h
@@ -0,0 +1,76 @@
+﻿/*
+ * <кодировка символов>
+ *   Cyrillic (UTF-8 with signature) - Codepage 65001
+ * </кодировка символов>
+ *
+ * <сводка>
+ *   CEcoLab1
+ * </сводка>
+ *
+ * <описание>
+ *   Данный заголовок описывает реализацию компонента CEcoLab1
+ * </описание>
+ *
+ * <автор>
+ *   Copyright (c) 2018 Vladimir Bashev. All rights reserved.
+ * </автор>
+ *
+ */
+
+#ifndef __C_ECOLAB2_H__
+#define __C_ECOLAB2_H__
+
+#include "IEcoSystem1.h"
+#include "IdEcoMemoryManager1.h"
+#include "IEcoLab1.h"
+#include "IEcoCalculatorY.h"
+#include "IEcoCalculatorX.h"
+
+typedef struct CEcoLab2 {
+
+    /* Таблица функций интерфейса IEcoCalculatorY */
+    IEcoCalculatorYVTbl* m_pVTblIY;
+
+    /* Таблица функций интерфейса IEcoCalculatorX */
+    IEcoCalculatorXVTbl* m_pVTblIX;
+
+    /* Неделигирующий интерфейс IEcoNondelegatingUnknown */
+    IEcoUnknownVTbl* m_pVTblINondelegatingUnk;
+
+    /* Счетчик ссылок */
+    uint32_t m_cRef;
+
+    /* Интерфейс для работы с памятью */
+    IEcoMemoryAllocator1* m_pIMem;
+
+    /* Системный интерфейс */
+    IEcoSystem1* m_pISys;
+
+    /* Данные экземпляра */
+    char_t* m_Name;
+
+    /* Ссылка на внутренний компонент для включения, может быть получен из D или E */
+    IEcoCalculatorY* m_pIY;
+
+    /* Ссылка на интерфейс IEcoUnknown внутреннего агрегируемого компонента Lab1 */
+    IEcoUnknown* m_pInnerUnknownLab1;
+
+    /* Ссылка на интерфейс IEcoUnknown внутреннего агрегируемого компонента B */
+    IEcoUnknown* m_pInnerUnknownB;
+
+    /* Ссылка на внутренний компонент для включения, может быть получен из A*/
+    IEcoCalculatorX* m_pIX;
+
+    /* Делегирующий IEcoUnknown, указывает на внешний или неделегирующий IEcoUnknown */
+    IEcoUnknown* m_pIUnkOuter;
+
+} CEcoLab2, *CEcoLab2Ptr;
+
+/* Инициализация экземпляра */
+int16_t ECOCALLMETHOD initCEcoLab2(/*in*/ struct IEcoUnknown * me, /* in */ IEcoUnknown *pIUnkSystem);
+/* Создание экземпляра */
+int16_t ECOCALLMETHOD createCEcoLab2(/* in */ IEcoUnknown* pIUnkSystem, /* in */ IEcoUnknown* pIUnkOuter, /* out */ IEcoUnknown** ppIEcoLab2);
+/* Удаление */
+void ECOCALLMETHOD deleteCEcoLab2(/* in */ IEcoCalculatorY* pIEcoLab2);
+
+#endif /* __C_ECOLAB2_H__ */
